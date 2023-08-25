849a3fbb95011de017ee9355dcf8c14f25825ad8
Sean Bradley <sb_210@hotmail.com>
2023-08-24 22:53:04 -0400

    Redux for All

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -1,7 +1,15 @@
 import { PROMOTIONS } from '../../app/shared/PROMOTIONS';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const selectFeaturedPromotion = () => {
-    return PROMOTIONS.find((promotion) => promotion.featured);  {/*finds first object where featured property equals true*/}
+const initialState = {
+    promotionsArray: PROMOTIONS
 };
 
-{/*Go to file for logic dealing with promotions data*/}
+const promotionSlice = createSlice({
+    name: 'promotions',
+    initialState
+});
+
+export const selectFeaturedPromotion = (state) => {
+    return state.promotions.promotionsArray.find((promotion) => promotion.featured);  {/*finds first object where featured property equals true*/}
+};
